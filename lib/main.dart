4299c5db85588348a4f21205c9da8cd8e3b7c6a8
Kenny Wong <kennywong8459@gmail.com>
2026-03-07 12:20:15 +0800

    initialize firebase and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/splash_screen.dart';
 import 'utils/colors.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -25,7 +31,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primaryBlue,
         scaffoldBackgroundColor: AppColors.backgroundColor,
-        //fontFamily: 'Poppins',
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: AppColors.textWhite),
           bodyMedium: TextStyle(color: AppColors.textWhite),
